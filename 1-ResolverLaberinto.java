650e1206a0c4ffca9079d76f386228c1266c7432
Rafael Alejandro Contreras Avalos <l20120970@morelia.tecnm.mx>
2024-02-06 22:26:41 -0600

    Resolver laberinto resuelto

--- 1-ResolverLaberinto.java
+++ 1-ResolverLaberinto.java
@@ -1,21 +1,65 @@
 
 public class ResolverLaberinto {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
         int[][] laberinto = {
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 1, 0, 0, 0, 0, 1, 1},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 1, 1, 0, 0, 0, 1, 1},
             {0, 1, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 1, 0, 0},
-            {0, 0, 1, 0, 0, 1, 1, 1},
-            {0, 1, 1, 1, 0, 0, 0, 0},
-            {0, 0, 1, 0, 0, 1, 1, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0}
+            {1, 0, 1, 0, 0, 1, 0, 1},
+            {0, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 3}
         };
         
-
+        recorrerLaberinto(laberinto, 0, 0);
+        
+        System.out.println("Solucion");
         imprimirMatriz(laberinto);
+    }
+    
+    
+    
+        public static boolean recorrerLaberinto(int[][] matriz, int fila, int columna){       
+            
+            //verificar que se accede a una ubicacion valida de la matriz
+            if(fila<matriz.length && columna<matriz[0].length && fila>=0 && columna >=0 && matriz[fila][columna]!=1 && matriz[fila][columna]!=2){
+                
+                if(matriz[fila][columna]==3){
+                    return true;
+                }    
+                
+                matriz[fila][columna]=2; 
+                
+                if(recorrerLaberinto(matriz, fila+1, columna)){
+                    return true;
+                }
+                
+                if(recorrerLaberinto(matriz, fila-1, columna)){
+                    return true;
+                }
+                
+                if(recorrerLaberinto(matriz, fila, columna+1)){
+                    return true;
+                }
+                
+                if(recorrerLaberinto(matriz, fila, columna-1)){
+                    return true;
+                }
+                
+          
+                
+                //Si se llega u un lugar sin salida se hace backtracking y se borra
+                //la celda de la solucion
+                matriz[fila][columna]=0;    
+                return false;
+                
+            }
+        return false;
     }
     
     
@@ -27,6 +71,7 @@ public class ResolverLaberinto {
             System.out.println();
         }
     }
+    
     
     
 }
